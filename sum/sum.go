afe2eea5007f65c47a78517bf6512269c47da557
VasanthaKumar <vasanthakumar@LAPSZ3359.ird.mu-sigma.com>
2019-02-01 20:26:02 +0530

    structs and interfaces

--- sum/sum.go
+++ sum/sum.go
@@ -1,11 +1,30 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(tailsToSum ...[]int) (sums []int) {
+	for _, numbers := range tailsToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return
 }
 
 func main() {}
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,14 +1,41 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
-
-	if want != got {
-		t.Errorf("got %d want %d for %v", got, want, numbers)
+	assertEqualSlices := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	}
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d for %v", got, want, numbers)
+		}
+	})
+
+	t.Run("returns the sum of the given list of ints", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		assertEqualSlices(t, got, want)
+	})
+
+	t.Run("returns the sum of tails of the given set of slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		assertEqualSlices(t, got, want)
+	})
 }

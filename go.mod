2edb710b41d7001a7181aaa5d237fa3cf2ce2fb5
VasanthaKumar <vasanthakumar@LAPSZ3359.ird.mu-sigma.com>
2019-02-04 17:08:19 +0530

    Pointers, errs and interface

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/VasanthakumarV/tdddev
+
+require (
+	github.com/kisielk/errcheck v1.2.0 // indirect
+	golang.org/x/tools v0.0.0-20190202235157-7414d4c1f71c // indirect
+)
--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -3,21 +3,21 @@ package perimeter
 import "testing"
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{"Rectange", &Rectange{12, 6}, 72.0},
+		{"Circle", &Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := &Rectange{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := &Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %.2f want %.2f", got, tt.want)
+			}
+		})
+	}
 }
